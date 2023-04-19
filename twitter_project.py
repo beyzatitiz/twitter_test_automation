a7c93497ced766c0082fd9b4194a3faaa450d909
Beyza Nur Titiz <122045140+beyzatitiz@users.noreply.github.com>
2023-04-19 12:39:44 +0300

    Update twitter_project.py

--- twitter_project.py
+++ twitter_project.py
@@ -15,29 +15,23 @@ class TwitterProject:
 
     def login_with_twitter(self):
         self.driver.get("https://twitter.com/?lang=tr")
-        twitter_login_button = self.driver.find_element(By.XPATH,
-                                                        '//*[@id="layers"]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/a/div/span/span')
+        twitter_login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/a/div/span/span')
         twitter_login_button.click()
         time.sleep(5)
-        name_input = self.driver.find_element(By.XPATH,
-                                              '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+        name_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
         name_input.click()
         name_input.send_keys(self.user_name)
-        continue_button = self.driver.find_element(By.XPATH,
-                                                   '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')
+        continue_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')
         continue_button.click()
         time.sleep(5)
-        pasword_input = self.driver.find_element(By.XPATH,
-                                                 '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        pasword_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
         pasword_input.send_keys(self.user_password)
-        login_button = self.driver.find_element(By.XPATH,
-                                                '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div')
+        login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div')
         login_button.click()
         time.sleep(15)
 
     def search_by_a_hashtag(self):
-        search_input = self.driver.find_element(By.XPATH,
-                                                '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input')
+        search_input = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input')
         search_input.send_keys(self.hashtag_word)
         search_input.send_keys(Keys.RETURN)
         time.sleep(10)
